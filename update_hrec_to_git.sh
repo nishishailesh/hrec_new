a1f7744a6df7ff872e6bfaf012d25704730e35c2
Your Name <you@example.com>
2022-05-24 12:39:22 +0530

    modified:   update_hrec_to_git.sh

--- update_hrec_to_git.sh
+++ update_hrec_to_git.sh
@@ -3,7 +3,7 @@ read $u
 echo 'give pass:'
 read $p
 
-mysqldump -d -u$u hrec_new -p$p > hrec_new_blank.sql 
+mysqldump -d -u$u -p$p hrec_new > hrec_new_blank.sql 
 git add *
 git commit
 git push https://github.com/nishishailesh/hrec_new main
